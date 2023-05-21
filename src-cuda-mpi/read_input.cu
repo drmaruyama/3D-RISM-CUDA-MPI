31a9913eeafbff483eb99bb58ac78b4e5b5348e5
Yutaka Maruyama <drmaruyama@gmail.com>
2023-05-21 20:49:47 +0900

    Deleted superfluous "int num;" in read_input.cu

--- src-cuda-mpi/read_input.cu
+++ src-cuda-mpi/read_input.cu
@@ -56,14 +56,11 @@ void RISM3D :: read_input (string control, string structure, bool centering) {
       cout << "reading solute data file : " << structure << endl;
     }
 
-    int num;
-
     string tmp;
     in_file >> tmp;
 
     if (isNaturalNumber(tmp)) {
       num = std::stoi(tmp);
-      cout << num << endl;
     } else {
       cout << "Error: Number of solute atom is not a natural number." << endl;
       exit(1);
